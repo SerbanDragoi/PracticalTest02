a56091ee74f042e57c656de3e89d619c94ef3a63
SerbanDragoi <serban.dragoi@stud.acs.upb.ro>
2023-05-25 15:14:23 +0300

    tasks 1-7

---
+++ Practical2Test/app/src/main/java/ro/pub/cs/systems/eim/practical2test/Constants.java
@@ -0,0 +1,13 @@
+package ro.pub.cs.systems.eim.practical2test;
+
+public class Constants {
+
+    final public static String TAG = "[PracticalTest02]";
+    final public static boolean DEBUG = true;
+
+
+
+
+    final public static String EMPTY_STRING = "";
+
+}
